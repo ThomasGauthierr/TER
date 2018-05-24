1c54f491ae467308370ec7a53be6ac188811d4ed
Ivan Picard Marchetto <ivan.picardmarchetto@gmail.com>
2018-05-24 15:09:54 +0200

    Added empty test

---
+++ src/test/java/core/behavior/context/ViolatedContextTest.java
@@ -0,0 +1,14 @@
+package core.behavior.context;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ViolatedContextTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/core/descision/SimplePriorityDescisionTest.java
@@ -0,0 +1,14 @@
+package core.descision;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimplePriorityDescisionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
