042d0c999719b517e351b461b7ae3b5df772b3fc
Couvreur Alexis <alexiscouvreur.pro@gmail.com>
2018-06-13 21:35:11 +0200

    clean device

--- src/main/java/core/device/Device.java
+++ src/main/java/core/device/Device.java
@@ -12,63 +12,43 @@ public abstract class Device {
 
     private String identifier;
     private SerialPort serialPort;
-    private DeviceReader deviceReader;
-    private DeviceWriter deviceWriter;
 
     public Device(String identifier, SerialPort serialPort) throws TooManyListenersException {
         this.identifier = identifier;
         this.serialPort = serialPort;
 
-        deviceReader = new DeviceReader();
-
-        serialPort.notifyOnDataAvailable(true);
-        serialPort.addEventListener(sp -> {
-            if (sp.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+        this.serialPort.addEventListener(sp -> {
+            if (sp.getEventType() == SerialPortEvent.DATA_AVAILABLE)
                 try {
-                    onDataAvailable(deviceReader.read());
+                    onDataAvailable(read());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }
         });
     }
 
-    private void onDataAvailable(String data) {
-        String[] values = data.split(" ");
-
-        if (values.length != 3)
-            return;
-
-        onDataRead(values[0], Double.valueOf(values[1]), Long.valueOf(values[2]));
+    public void setReadDataWhenAvailable(boolean readDataWhenAvailable) {
+        this.serialPort.notifyOnDataAvailable(readDataWhenAvailable);
     }
 
-    protected abstract void onDataRead(String source, double value, long timestamp);
+    protected abstract void onDataAvailable(String data);
 
-    protected abstract void onDataWritten(String destination, double value, long timestamp);
+    protected String read() throws IOException {
+        StringBuilder data = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+        String line;
+        while ((line = br.readLine()) != null) {
+            data.append(line);
+        }
+        return data.toString();
+    }
 
-    public void write(String message) throws IOException {
-        deviceWriter.write(message);
+    protected void write(String message) throws IOException {
+        serialPort.getOutputStream().write((message + "\n").getBytes());
     }
 
     public String getIdentifier() {
         return identifier;
     }
 
-    private class DeviceWriter {
-        void write(String message) throws IOException {
-            serialPort.getOutputStream().write((message + "\n").getBytes());
-        }
-    }
-
-    private class DeviceReader {
-        public String read() throws IOException {
-            StringBuilder data = new StringBuilder();
-            BufferedReader br = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
-            String line;
-            while ((line = br.readLine()) != null) {
-                data.append(line);
-            }
-            return data.toString();
-        }
-    }
 }
