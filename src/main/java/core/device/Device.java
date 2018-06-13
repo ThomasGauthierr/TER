d99734f9b1a5b73e8aefa17caf02382155725493
Couvreur Alexis <alexiscouvreur.pro@gmail.com>
2018-06-13 16:34:42 +0200

    New device

--- src/main/java/core/device/Device.java
+++ src/main/java/core/device/Device.java
@@ -1,45 +1,74 @@
 package core.device;
 
-import core.influxdb.InfluxHelper;
 import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Observable;
+import java.io.InputStreamReader;
+import java.util.TooManyListenersException;
 
-public abstract class Device extends Observable implements IDevice {
-    private String ID;
+public abstract class Device {
+
+    private String identifier;
     private SerialPort serialPort;
-    protected InfluxHelper influxHelper;
+    private DeviceReader deviceReader;
+    private DeviceWriter deviceWriter;
 
-    public Device(String ID, SerialPort serialPort) {
-        this.ID = ID;
+    public Device(String identifier, SerialPort serialPort) throws TooManyListenersException {
+        this.identifier = identifier;
         this.serialPort = serialPort;
-        //this.influxHelper = InfluxHelper.getInfluxHelper();
+
+        deviceReader = new DeviceReader();
+
+        serialPort.notifyOnDataAvailable(true);
+        serialPort.addEventListener(sp -> {
+            if (sp.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+                try {
+                    onDataAvailable(deviceReader.read());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
-    @Override
-    public String getID() {
-        return ID;
+    private void onDataAvailable(String data) {
+        String[] values = data.split(" ");
+
+        if (values.length != 3)
+            return;
+
+        onDataRead(values[0], Double.valueOf(values[1]), Long.valueOf(values[2]));
     }
 
-    @Override
-    public SerialPort getSerialPort() {
-        return serialPort;
+    protected abstract void onDataRead(String source, double value, long timestamp);
+
+    protected abstract void onDataWritten(String destination, double value, long timestamp);
+
+    public void write(String message) throws IOException {
+        deviceWriter.write(message);
     }
 
-    @Override
-    public void writeString(String s) {
-        try {
-            getSerialPort().getOutputStream().write((s + "\n").getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    private class DeviceWriter {
+        void write(String message) throws IOException {
+            serialPort.getOutputStream().write((message + "\n").getBytes());
         }
     }
 
-    @Override
-    public String toString() {
-        return "Device{" +
-                "ID='" + ID + '\'' +
-                '}';
+    private class DeviceReader {
+        public String read() throws IOException {
+            StringBuilder data = new StringBuilder();
+            BufferedReader br = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+            String line;
+            while ((line = br.readLine()) != null) {
+                data.append(line);
+            }
+            return data.toString();
+        }
     }
 }
