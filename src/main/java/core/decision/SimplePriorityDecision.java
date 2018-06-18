c3bb97632becdc156cf742fa89bb5be93a86eaf2
Ivan Picard Marchetto <ivan.picard-marchetto@etu.unice.fr>
2018-06-18 14:57:52 +0200

    upgraded decision

--- src/main/java/core/decision/SimplePriorityDecision.java
+++ src/main/java/core/decision/SimplePriorityDecision.java
@@ -39,7 +39,7 @@ public class SimplePriorityDecision implements IDecisionMaker {
 		ctx.getViolatingMessage().getValue();
 		List<IActuator> responsible = new ArrayList<>();
 		ctx.getMonitoredEntities();
-		boolean toohigh = violatedContract.getArithmeticCondition()==ArithmeticCondition.LOWER_THAN 
+		final boolean toohigh = violatedContract.getArithmeticCondition()==ArithmeticCondition.LOWER_THAN 
 				||violatedContract.getArithmeticCondition()==ArithmeticCondition.LOWER_THAN_OR_EQUAL_TO;
 		for(IActuator a : source.getActuators()){
 			if(a.getDataType()==dT){
@@ -51,11 +51,36 @@ public class SimplePriorityDecision implements IDecisionMaker {
 			@Override
 			public int compare(IActuator o1, IActuator o2) {
 				Annuaire annuaire = Annuaire.getInstance();
-
+				if(toohigh && o1.getActionType()!=o2.getActionType()) {
+					if(o1.getActionType()==ActionType.INCREASE) {
+						return - 1;
+					}
+					else {
+						return 1;
+					}
+				}else if(!toohigh && o1.getActionType()!=o2.getActionType()) {
+					if(o1.getActionType()==ActionType.DECREASE) {
+						return - 1;
+					}
+					else {
+						return 1;
+					}
+				}
 				return annuaire.getInformationAbout(o2.getIdentifier()).getPriority() - annuaire.getInformationAbout(o1.getIdentifier()).getPriority();
 			}
 		});
-		action.actuators.addAll(responsible);
+		for(IActuator a : responsible){
+			if(a.getActionType().equals(ActionType.INCREASE) && toohigh) {
+				if(a.getState()!=IActuator.State.OFF)
+					action.actuators.add(a);
+			}else if(a.getActionType().equals(ActionType.DECREASE) && !toohigh){
+				if(a.getState()!=IActuator.State.OFF)
+					action.actuators.add(a);
+			}else{
+				if(a.getState()!=IActuator.State.ON)
+					action.actuators.add(a);
+			}
+		}
 		for(IActuator a : action.actuators){
 			if(a.getActionType().equals(ActionType.INCREASE) && toohigh) {
 				action.states.add(IActuator.State.OFF);
